0780afa3df83987161cdce01c6446cfbe4166303
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-02-04 18:57:07 +0200

    Add env variable for api url

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 
 import AppNavigation from './navigation/AppNavigation.jsx';
 import { NavigationContainer } from '@react-navigation/native';
+import 'dotenv/config';
 
 export default function App() {
   return (
